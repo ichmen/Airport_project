b7bfc26d42540dd9924113fa07d1cefb2235e317
ichmen <ichmen@rambler.ru>
2022-10-03 00:23:08 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import { Provider } from 'react-redux';
 import Dashboard from './dashboard/components/Dashboard';
 import { store } from './dashboard/actions/dashboard.store';
 import DashboardMode from './dashboard/components/DashboardMode';
+import Calendar from 'react-calendar';
+import CalendarWrap from './dashboard/components/calendar/CalendarWrap';
 
 function App() {
   return (
     <Provider store={store}>
       <DashboardMode />
+      {/* <Calendar locale={'en-EN'} className={'flights__calendar'} /> */}
+      <CalendarWrap />
       <Dashboard />
     </Provider>
   );
---
+++ src/dashboard/components/calendar/CalendarWrap.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
+import { calendarDatesToShow } from './calendar.utils';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+export default function CalendarWrap() {
+  const { today, yesterday, tomorrow } = calendarDatesToShow();
+  const [showCalendar, setCalendarVisibility] = useState(false);
+
+  return (
+    <ul className="calendar">
+      <li className="calendar__item calendar__item_active">
+        <span className="calendar__item__date">{today}</span>
+        <FontAwesomeIcon
+          icon={faCalendar}
+          className="calendar__icon"
+          onClick={() => setCalendarVisibility(!showCalendar)}
+        />
+        {showCalendar && <Calendar className={'flights__calendar'} locale={'en-EN'} />}
+      </li>
+      <li className="calendar__item">
+        <span className="calendar__item__date">{yesterday}</span>
+        <span className="calendar__item__day">yesterday</span>
+        <hr className="calendar__item__line"></hr>
+      </li>
+      <li className="calendar__item">
+        <span className="calendar__item__date">{today}</span>
+        <span className="calendar__item__day">today</span>
+        <hr></hr>
+      </li>
+      <li className="calendar__item">
+        <span className="calendar__item__date">{tomorrow}</span>
+        <span className="calendar__item__day">tomorrow</span>
+        <hr></hr>
+      </li>
+    </ul>
+  );
+}
