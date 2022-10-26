e7d9c53575e48aa2c3f2d019043a806b307bc7c5
ichmen <ichmen@rambler.ru>
2022-10-26 09:26:18 +0300

    ports

--- src/gateway/gateway.js
+++ src/gateway/gateway.js
@@ -3,6 +3,7 @@ import { formatDate } from '../utils/utils';
 import flightsPromise from '../10-9-2021';
 
 export function fetchFlights(date = new Date()) {
-  return fetch(baseUrl + formatDate(date)).then(response => response.json());
-  // .catch(() => flightsPromise); //closed ports case
+  return fetch(baseUrl + formatDate(date))
+    .then(response => response.json())
+    .catch(() => flightsPromise); //closed ports case
 }
